e2bf5aaf6ebd1feac0448793af600af7b403bc4a
Alexander <pchp250g8@tutamail.com>
2026-01-22 14:15:14 +0300

    Добавьте файлы проекта.

---
+++ ProductOfElems/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim nElems = 0
+        Console.Clear()
+        Console.WriteLine("Input a count of elements")
+        Int32.TryParse(Console.ReadLine(), nElems)
+        Dim iArray(nElems) As Short
+        Dim oRnd = New Random()
+        For i = 0 To nElems - 1
+            Dim nItem = oRnd.Next(1, 10)
+            iArray(i) = nItem
+        Next
+        Dim nProduct = 1L
+        For i = 0 To nElems - 1
+            nProduct *= iArray(i)
+            Console.Write(Str(iArray(i)) + " ")
+        Next
+        Console.WriteLine(vbCrLf + $"The Product of {nElems} elements of the array is:{nProduct}")
+        Console.Read()
+    End Sub
+
+End Module
---
+++ ProductOfElems/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("ProductOfElems")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ProductOfElems")>
+<Assembly: AssemblyCopyright("Copyright ©  2026")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("2e3d975b-4940-4200-aa74-b9a9f6d0e726")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
